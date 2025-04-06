97915e5af2a3b8b7088b6b3e2e25523e53957bba
udhayasooriyan <udhayasooriyan@wl.team>
2025-04-06 16:07:26 +0530

    feat: Implement authentication handling in DataContext with login/logout functions

--- FE/src/context/DataContext.jsx
+++ FE/src/context/DataContext.jsx
@@ -1,11 +1,12 @@
 import { createContext, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import Api from "../api/Api";
 
 const DataContext = createContext({});
 
 export const DataProvider = ({ children }) => {
     const [loggedUser, setLoggedUser] = useState("");
-    const [token, setToken] = useState("");
+    const [token, setToken] = useState(null);
     const [resetToken, setResetToken] = useState("");
     const [isLoading, setIsLoading] = useState(false);
     const navigate = useNavigate();
@@ -15,17 +16,43 @@ export const DataProvider = ({ children }) => {
         },
     });
 
-
     // states for canvas
 
     const [color, setColor] = useState("#000");
     const [size, setSize] = useState(2);
 
 
+    useEffect(() => {
+        let authToken = localStorage.getItem("authToken");
+        if (authToken) setToken(authToken);
+    }, []);
+
+
+
+
+    const handleLogin = async (email, password) => {
+        try {
+            const payload = { email, password };
+            const response = await Api.post("/auth/login", payload);
+            setToken(response.data.token);
+            setLoggedUser(response.data.user);
+            localStorage.setItem("authToken", response.data.token);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const handleLogout = () => {
+        localStorage.removeItem("authToken");
+        setToken(null);
+        setLoggedUser("");
+    };
+
+
 
     return (
         <DataContext.Provider
-            value={{ color, setColor, size, setSize }}
+            value={{ color, setColor, size, setSize, handleLogin, handleLogout, token, loggedUser }}
         >
             {children}
         </DataContext.Provider>
